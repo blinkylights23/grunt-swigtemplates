a027b44e1782b2693a229205517bf6ed0b39bbe6
Paul Smith <paul@blinkylights.org>
2014-09-09 14:33:39 -0400

    I18n refactoring

--- tasks/swigtemplates.js
+++ tasks/swigtemplates.js
@@ -2,7 +2,7 @@
  * grunt-swigtemplates
  * https://github.com/blinkylights23/grunt-swigtemplates
  *
- * Copyright (c) 2013 Paul Smith
+ * Copyright (c) 2014 Paul Smith
  * Licensed under the MIT license.
  */
 
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
       _ = require('lodash'),
       path = require('path');
 
+  var Q = require('q');
 
   var inspect = function(obj) {
     var inspectOpts = {
@@ -29,14 +30,6 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('swigtemplates', 'Grunt plugin for working with swig templates.', function(target) {
 
-
-    // Context precedence order
-    // 1. global.json
-    // 2. myfile.ext.json
-    // 3. options: { defaultContext: {} }
-    // 4. target: { context: {} }
-
-
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       autoEscape: true,
@@ -46,8 +39,8 @@ module.exports = function(grunt) {
       locals: {},
       locales: [],
       defaultLocale: undefined,
-      translateFunction: function(locale, msg) { return msg; },
-      translateFunctionName: '__', 
+      translateFunction: function(locale) { return function(msg) { return msg; } },
+      translateFunctionName: '__',
       filters: {},
       defaultContext: {},
       templatesDir: '.'
@@ -59,9 +52,6 @@ module.exports = function(grunt) {
     if (options.locales.length > 0) { useI18n = true; }
     if (useI18n && options.defaultLocale && options.locales.indexOf(options.defaultLocale) === -1) {
       grunt.log.error('Default locale ' + options.defaultLocale + ' not in configured locales: ' + inspect(options.locales));
-    }
-    if (!useI18n) {
-      swig.setDefaults({ locals: options.locals });
     }
 
     // Configure swig
@@ -78,65 +68,102 @@ module.exports = function(grunt) {
       swig.setFilter(key, options.filters[key]);
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
+    // Hold on to your butts
+    if (!useI18n) {
+      handleFiles(this.files);
+    } else {
+      handleI18nFiles(this.files);
+    }
 
-      var src = f.src.filter(function(filepath) {
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-
-        var outfileName = path.basename(filepath, '.swig'),
-            dirName = path.dirname(filepath),
-            outfilePath = path.normalize(path.relative(options.templatesDir, dirName)),
-            outfile = path.join(f.dest, outfilePath, outfileName);
-
-        // Get context
-        var globalContext,
-            templateContext;
-
-        try {
-          globalContext = grunt.file.readJSON(path.join(options.templatesDir, "global.json"));
-        } catch(err) {
-          grunt.log.warn('No global.json: ', err);
-          globalContext = {};
-        }
-        try {
-          templateContext = grunt.file.readJSON(path.join(dirName, outfileName) + ".json");
-        } catch(err) {
-          templateContext = {};
-        }
-        var context = _.extend({}, globalContext, templateContext, options.defaultContext, f.context);
-
-        // Write the destination file(s).
-        if (useI18n) {
-          context.locales = options.locales;
-          options.locales.forEach(function(l) {
-            var localizedOutFile = outfile;
-            if (l !== options.defaultLocale) {
-              localizedOutFile = path.join(f.dest, l, outfilePath, outfileName);
-            }
-            context.locale = l;
-            options.locals[options.translateFunctionName] = function() {
-              var args = Array.prototype.slice.call(arguments);
-              return options.translateFunction(l, args);
-            };
-            swig.setDefaults({ locals: options.locals });
-            grunt.file.write(localizedOutFile, swig.renderFile(filepath, context));
-            grunt.log.ok('File "' + localizedOutFile + '" created.');
-          });
-        } else {
-          grunt.file.write(outfile, swig.renderFile(filepath, context));
-          grunt.log.ok('File "' + outfile + '" created.');
-        }
-
+    // Handle non-i18n files
+    function handleFiles(files) {
+      files.forEach(function(f) {
+        f.src.filter(srcExists).map(function(filepath) {
+          var pathInfo = getPathInfo(filepath, f.dest),
+              context = getContext(f.context, pathInfo);
+          renderFile(pathInfo.outfile, filepath, context);
+        });
       });
+    };
 
-    });
+    // Handle i18n files
+    function handleI18nFiles(files) {
+      options.locales.forEach(function(locale) {
+        grunt.registerTask('swigtemplatesSubtask-' + locale, function() {
+          var done = this.async();
+          var translatorFactory = options.translateFunction(locale);
+          Q.when(translatorFactory, function(translator) { doTranslations(files, locale, translator) }).done(done);
+        });
+        grunt.task.run('swigtemplatesSubtask-' + locale);
+      });
+    }
+
+    // Do translations
+    function doTranslations(files, locale, translator) {
+      files.forEach(function(f) {
+        f.src.filter(srcExists).map(function(filepath) {
+          var pathInfo = getPathInfo(filepath, f.dest),
+              context = getContext(f.context, pathInfo);
+          if (locale !== options.defaultLocale) {
+            pathInfo.outfile = path.join(f.dest, locale, pathInfo.outfilePath, pathInfo.outfileName);
+          }
+          context.locale = locale;
+          options.locals[options.translateFunctionName] = function() {
+            var args = Array.prototype.slice.call(arguments);
+            return translator(args);
+          };
+          swig.setDefaults({ locals: options.locals });
+          renderFile(pathInfo.outfile, filepath, context);
+        });
+      });
+    }
+
+    // Render file
+    function renderFile(outfile, filepath, context) {
+      grunt.file.write(outfile, swig.renderFile(filepath, context));
+      grunt.log.ok('File "' + outfile + '" created.');
+    }
+
+    // Get path info for src/dest
+    function getPathInfo(filepath, dest) {
+      var outfileName = path.basename(filepath, '.swig'),
+        dirName = path.dirname(filepath),
+        outfilePath = path.normalize(path.relative(options.templatesDir, dirName)),
+        outfile = path.join(dest, outfilePath, outfileName);
+      return { outfileName: outfileName, dirName: dirName, outfilePath: outfilePath, outfile: outfile };
+    }
+
+    // Get swig context
+    function getContext(context, pathInfo) {
+      var globalContext,
+          templateContext;
+
+      console.log(pathInfo);
+
+      try {
+        globalContext = grunt.file.readJSON(path.join(options.templatesDir, "global.json"));
+      } catch(err) {
+        console.log('Error: ', err);
+        globalContext = {};
+      }
+      try {
+        templateContext = grunt.file.readJSON(path.join(pathInfo.dirName, pathInfo.outfileName) + ".json");
+      } catch(err) {
+        console.log('Error: ', err);
+        templateContext = {};
+      }
+      return _.extend({}, globalContext, templateContext, options.defaultContext, context);
+    }
+
+    // Src file exists
+    function srcExists(filepath) {
+      if (!grunt.file.exists(filepath)) {
+        grunt.log.warn('Source file "' + filepath + '" not found.');
+        return false;
+      } else {
+        return true;
+      }
+    }
 
   });
 
